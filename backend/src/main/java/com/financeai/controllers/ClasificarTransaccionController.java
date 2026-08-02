e0db4556a044a1be9807fc4e4e70ffa30cb2a069
Maxi <maximofran9@gmail.com>
2026-08-01 21:20:47 -0300

    Backend: Agregado de imports

--- backend/src/main/java/com/financeai/controllers/ClasificarTransaccionController.java
+++ backend/src/main/java/com/financeai/controllers/ClasificarTransaccionController.java
@@ -3,6 +3,8 @@ package com.financeai.controllers;
 import com.financeai.dtos.*;
 import com.financeai.services.*;
 
+import jakarta.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
